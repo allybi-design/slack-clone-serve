8449a13fdec72f408520858c1a41a5d4539e9205
node-toso-api <alisonsmithaccy@gmail.com>
2019-09-08 13:40:17 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Chatkit = require("@pusher/chatkit-server");
+
+const app = express();
+
+const chatkit = new Chatkit.default({
+  instanceLocator: "v1:us1:8b02d032-1a3e-49ca-b6b2-a11cfa99f3f0",
+  key:
+    "3914285e-db4c-445a-b3bd-2811d7df66e4:zcfZmo0Wstmgn/lw7KthPI8DSIIMTCLiDWkbEztE0i4="
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/users", (req, res) => {
+  const { username } = req.body;
+  chatkit
+    .createUser({
+      id: username,
+      name: username
+    })
+    .then(() => res.sendStatus(201))
+    .catch(error => {
+      if (error.error === "services/chatkit/user_already_exists") {
+        res.sendStatus(200);
+      } else {
+        res.status(error.status).json(error);
+      }
+    });
+});
+
+app.post("/authenticate", (req, res) => {
+  const authData = chatkit.authenticate({ userId: req.query.user_id });
+  res.status(authData.status).send(authData.body);
+});
+
+const PORT = 3001;
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Running on port ${PORT}`);
+  }
+});
